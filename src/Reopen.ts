df67e79841316488e6b8d96729405e5765998403
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-31 18:04:56 +0300

    tsc

---
+++ src/Reopen.ts
@@ -0,0 +1,17 @@
+
+interface User {
+    name: string;
+    email: string;
+    password: string;
+}
+interface User{
+    telephone: string;
+}
+
+let Qasem: User={
+    name: "John Doe",
+    email: "johndoe@example.com",
+    password: "123456",
+    telephone: "123456",
+}
+console.log(Qasem)
--- src/interfacetypscript.ts
+++ src/interfacetypscript.ts
@@ -1,31 +1,31 @@
-interface User {
-    name: string;
-    id: number;
-    country: string,
-    age: number;
-    address: string;
-
-    sayHello(): string,
-
-    sayWelcome(): string
-
-    getDouble(num: number): number;
-}
-
-let user: User = {
-    name: "Qasem", id: 12, country: "Syria", age: 30, address: "New York No. 1 Lake Park", sayHello() {
-        return `Hello ${user.name}`;
-    },
-    sayWelcome() {
-        return `Welcome ${user.name}`;
-    },
-    getDouble(n) {
-        return n * 5;
-    }
-}
-
-console.log(user.id)
-console.log(user.name)
-console.log(user.sayHello())
-console.log(user.sayWelcome())
-console.log(user.getDouble(100))
+// interface User {
+//     name: string;
+//     id: number;
+//     country: string,
+//     age: number;
+//     address: string;
+//
+//     sayHello(): string,
+//
+//     sayWelcome(): string
+//
+//     getDouble(num: number): number;
+// }
+//
+// let user: User = {
+//     name: "Qasem", id: 12, country: "Syria", age: 30, address: "New York No. 1 Lake Park", sayHello() {
+//         return `Hello ${user.name}`;
+//     },
+//     sayWelcome() {
+//         return `Welcome ${user.name}`;
+//     },
+//     getDouble(n) {
+//         return n * 5;
+//     }
+// }
+//
+// console.log(user.id)
+// console.log(user.name)
+// console.log(user.sayHello())
+// console.log(user.sayWelcome())
+// console.log(user.getDouble(100))
