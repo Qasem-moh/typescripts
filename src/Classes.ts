3583e47833ee57c76fb7cdcce50f9bd706fe6d5d
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-02-01 16:08:47 +0300

    ts

--- src/Classes.ts
+++ src/Classes.ts
@@ -1,14 +1,18 @@
 class User {
 
     msg: () => string;
-
+    private static created: number = 0;
     constructor(private _username: string, protected salary: number) {
-
+        User.created++;
         this.msg = () => {
             return `hello ${this._username} your salary's ${this.salary}`;
         }
     }
 
+    getCreated(): void {
+        console.log(`count object ${User.created}`)
+
+    }
     sayMsg() {
         return `Hello, ${this._username} your salary is ${this.salary}`;
     }
@@ -28,3 +32,4 @@ console.log(userOne.username)
 // console.log(userOne.salary)
 console.log(userOne.msg())
 console.log(userOne.sayMsg())
+userOne.getCreated()
